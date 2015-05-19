37118d19ac29b36fbcd7d5527b5a95979af30147
Ian MacDonald & James Williams <student@epicodus.com>
2015-05-19 15:12:47 -0700

    Add validation for name and uniqueness of name for patient.

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,5 @@
 class Patient < ActiveRecord::Base
   has_many :visits
   has_many :specialists, through: :visits
+  validates :name, { presence: true, uniqueness: { case_sensitive: false } }
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe(Patient) do
-  it { should have_many (:visits)}
+  it { should have_many :visits }
+
+  it { should validate_presence_of :name }
+
+  it { should validate_uniqueness_of :name }
+
+  it { should use_before_action :titleize_name }
 end
