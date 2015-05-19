b03a25412825ee672d44ad87b961cd7bc9369e8d
ac & jc <student@epicodus.com>
2015-05-19 16:18:08 -0700

    stupid commit

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,10 +2,6 @@ class Patient < ActiveRecord::Base
   has_many :visits
   has_one :schedule, through: :visits
   has_many :specialists, through: :visits
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> specialist
   has_one :trial, through: :visits
   validates :name, { presence: true, uniqueness: { case_sensitive: false } }
   before_save :titleize_name
@@ -14,9 +10,5 @@ private
   def titleize_name
     self.name = name.downcase.titleize
   end
-<<<<<<< HEAD
-=======
 
->>>>>>> 0b2622d165ecc35866d6781f4b575e8698d304f9
->>>>>>> specialist
 end
