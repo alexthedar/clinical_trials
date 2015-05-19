66f61d28fe0a67eea26bb98cb756f202b8d953ec
Ian MacDonald & James Williams <student@epicodus.com>
2015-05-19 11:05:33 -0700

    Add sinatra route for generating ical ics file.

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'bundler/setup'
+Bundler.require :default
+
+get '/events/export' do
+  cal = Icalendar::Calendar.new
+  cal.event do |e|
+    e.dtstart     = Icalendar::Values::Date.new('20050428')
+    e.dtend       = Icalendar::Values::Date.new('20050429')
+    e.summary     = "Meeting with the man."
+    e.description = "Have a long lunch meeting and decide nothing..."
+    e.ip_class    = "PRIVATE"
+  end
+  cal.publish
+  Dir[File.dirname(__FILE__) + '/views/events.ics'].each do |file|
+    output = File.open( file, "w" )
+    output << cal.to_ical
+    output.close
+  end
+  erb :events
+end
