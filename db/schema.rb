9e54224768ff46e4034090a4514b813ec717629f
Ian MacDonald & James Williams <student@epicodus.com>
2015-05-20 10:34:30 -0700

    Update patient to have first_name and last_name.

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519223753) do
+ActiveRecord::Schema.define(version: 20150520162902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "patients", force: :cascade do |t|
-    t.string "name"
     t.string "phone"
     t.string "email"
     t.string "gender"
     t.date   "birthday"
+    t.string "first_name"
+    t.string "last_name"
   end
 
   create_table "schedules", force: :cascade do |t|
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -6,13 +6,26 @@ describe(Patient) do
   it { should have_one (:schedule) }
   it { should have_one (:trial)}
 
-  it { should validate_presence_of :name }
-  it { should validate_uniqueness_of :name }
+  describe 'validations' do
+    subject { Patient.new(first_name: 'john', last_name: 'connor') }
+    it { should validate_presence_of :last_name }
+    it { should validate_uniqueness_of :first_name }
+    it { should validate_presence_of :first_name }
+    it { should validate_uniqueness_of :last_name }
+    end
+
 
   describe '#titleize_name' do
     it 'takes a name and turns it to title case' do
-      patient = Patient.create(name: 'john connor')
-      expect(patient.name).to(eq("John Connor"))
+      patient = Patient.create(first_name: "john", last_name: "connor")
+      expect(patient.name).to(eq("Connor, John"))
+    end
+  end
+
+  describe '#name' do
+    it 'provdes the last and first name of patient' do
+      patient = Patient.create(first_name: "john", last_name: "connor")
+      expect(patient.name).to(eq("Connor, John"))
     end
   end
 end
