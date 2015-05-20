ed810f63d593d513f1a84701acbad2404ce809d8
Alex Caste <alex.castaneda@gmail.com>
2015-05-20 16:42:38 -0700

    add sm_del_partial, vacation, vacation_partial files.  in progress on vacations.  can add vacations.  no test for delete or patch.  unable to access vacations page

--- app.rb
+++ app.rb
@@ -77,35 +77,58 @@ get '/specialist/:id' do
   erb :specialist
 end
 
-post '/specialist/:id' do
-  @specialist = Specialist.find(params.fetch('id'))
-  first_name = params.fetch('first_name')
-  last_name = params.fetch('last_name')
-  phone = params.fetch('phone')
-  email = params.fetch('email')
-
-  if first_name == ""
-    first_name = @specialist.first_name
-  end
-  if last_name == ""
-    last_name = @specialist.last_name
-  end
-  if phone == ""
-    phone = @specialist.phone
-  end
-  if email == ""
-    email = @specialist.email
-  end
-  @specialist.update({:first_name => first_name, :last_name => last_name, :phone => phone, :email => email})
-
-  redirect '/specialist/'.concat(@specialist.id().to_s())
+patch '/specialist/:id' do
+  specialist = Specialist.find(params['id'])
+  specialist.update(first_name: params.fetch('first_name'))
+  specialist.update(last_name: params.fetch('last_name'))
+  specialist.update(phone: params.fetch('phone'))
+  specialist.update(email: params.fetch('email'))
+  redirect "/specialist/#{specialist.id}"
 end
+
+
+# patch '/specialist/:id' do
+#   @specialist = Specialist.find(params.fetch('id'))
+#   first_name = params.fetch('first_name')
+#   last_name = params.fetch('last_name')
+#   phone = params.fetch('phone')
+#   email = params.fetch('email')
+#
+#   if first_name == ""
+#     first_name = @specialist.first_name
+#   end
+#   if last_name == ""
+#     last_name = @specialist.last_name
+#   end
+#   if phone == ""
+#     phone = @specialist.phone
+#   end
+#   if email == ""
+#     email = @specialist.email
+#   end
+#   @specialist.update({:first_name => first_name, :last_name => last_name, :phone => phone, :email => email})
+#
+#   redirect '/specialist/'.concat(@specialist.id().to_s())
+# end
+
+get '/vacation/:id' do
+  @specialist = Specialist.find(params.fetch('id'))
+  @vacations = @specialist.vacations
+  erb(:vacation)
+end
+
+get '/vacation/add/:id' do
+  @specialist = Specialist.find(params.fetch('id'))
+  @vacations = @specialist.vacations(@specialist.id)
+  erb(:vacation_form)
+end
+
 
 post '/vacation/add/:id' do
   @specialist = Specialist.find(params.fetch('id'))
   @vacations = @specialist.vacations
-  start_date = params.fetch('start_date')
-  erb(:vacation_form)
+  new_vacation = Vacation.create({start_date: params['start_date'], end_date: ['end_date'], specialist_id:['id'] })
+  redirect "/vacation/#{@specialist.id}"
 end
 
 get '/trials' do
