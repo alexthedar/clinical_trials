941b13b23ebb9ac253948df74c2e8c0e6e9ae960
Alex Caste <alex.castaneda@gmail.com>
2015-05-21 11:26:43 -0700

    something broken cannot save changes

--- app.rb
+++ app.rb
@@ -86,49 +86,26 @@ patch '/specialist/:id' do
   redirect "/specialist/#{specialist.id}"
 end
 
-
-# patch '/specialist/:id' do
-#   @specialist = Specialist.find(params.fetch('id'))
-#   first_name = params.fetch('first_name')
-#   last_name = params.fetch('last_name')
-#   phone = params.fetch('phone')
-#   email = params.fetch('email')
-#
-#   if first_name == ""
-#     first_name = @specialist.first_name
-#   end
-#   if last_name == ""
-#     last_name = @specialist.last_name
-#   end
-#   if phone == ""
-#     phone = @specialist.phone
-#   end
-#   if email == ""
-#     email = @specialist.email
-#   end
-#   @specialist.update({:first_name => first_name, :last_name => last_name, :phone => phone, :email => email})
-#
-#   redirect '/specialist/'.concat(@specialist.id().to_s())
-# end
-
 get '/vacation/:id' do
   @specialist = Specialist.find(params.fetch('id'))
   @vacations = @specialist.vacations
+  binding.pry
   erb(:vacation)
-end
-
-get '/vacation/add/:id' do
-  @specialist = Specialist.find(params.fetch('id'))
-  @vacations = @specialist.vacations(@specialist.id)
-  erb(:vacation_form)
 end
 
 post '/vacation/add/:id' do
   @specialist = Specialist.find(params.fetch('id'))
-  @vacations = @specialist.vacations
   new_vacation = Vacation.create({start_date: params['start_date'], end_date: ['end_date'], specialist_id:['id'] })
+  @specialist.vacations.push(new_vacation)
   redirect "/vacation/#{@specialist.id}"
 end
+
+delete '/vacation/:id' do
+  vacation = Vacation.find(params.fetch('id'))
+  vacation.delete
+  redirect "/vacation/#{@specialist.id}"
+end
+
 
 get '/trials' do
   @alltrials = Trial.all
@@ -155,7 +132,6 @@ get '/trials/:id' do
 end
 
 post '/trials/:id/update' do
-  binding.pry
   @trial = Trial.find(params.fetch('id'))
   company = params.fetch('company')
   name = params.fetch('name')
