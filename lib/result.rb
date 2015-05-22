d826ef1f07281bd276a81aec7c96a3ea86fafd38
Ian MacDonald & James Williams <imacd20@gmail.com>
2015-05-22 12:21:38 -0700

    Started work on refactoring for schedule method in trial.

--- lib/result.rb
+++ lib/result.rb
@@ -1,20 +1,29 @@
-class Results
-  attr_reader(:conflicts, :warnings, :scheduled_visits)
+class Result
+  attr_reader(:conflict, :warning, :scheduled_visit)
 
   def initialize(results)
-    # @conflicts = attributes[:conflicts]
-    # @warnings = attributes[:warnings]
-    # @scheduled_visits = attributes[:scheduled_visits]
-    @results = parse(results)
+    @conflicts = Conflict.new(attributes[:conflicts]).parse
+    @warnings = Warning.new(attributes[:warnings]).parse
+    @scheduled_visits = Struct.new(attributes[:scheduled_visits]).parse
+    # @results = parse(results)
   end
 
-  Conflict = Struct.new(:visit_conflict, :conflict_reason, :date)
+  Conflict = Struct.new(:visit_conflict, :conflict_reason, :date) do
+    def initialize
+      @visit_conflict = attributes[:visit_conflict]
+    end
+  end
+
   Warning = Struct.new(:visit_conflict, :warning_reason, :date)
+
+
+
+
   Scheduled_Visit = Struct.new(:visit)
 
-  def parse(results)
-    results.collect do |cell|
-binding.pry      
+  def parse(messages)
+    messages.collect do |cell|
+binding.pry
       Conflict.new(cell[0], cell[1], cell[2])
       Warning.new(cell[0], cell[1], cell[2])
       Scheduled_Visit.new(cell[0])
