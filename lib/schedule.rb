d710c7ccb8fded8e2f61e8e82fd7410d6012ef8d
Alex Caste <alex.castaneda@gmail.com>
2015-05-22 12:20:39 -0700

    add delete and update function to schedule table.  working but bug is editable any where table is published.  needs to be isolated ot the edit schedule page

--- lib/schedule.rb
+++ lib/schedule.rb
@@ -4,4 +4,9 @@ class Schedule < ActiveRecord::Base
   has_many :patients, through: :visits
   has_many :specialists, through: :visits
 
+  def delete_schedule(trial_id, visit_number)
+    Schedule.delete("visit_number = #{visit_number} AND trial_id = #{trial_id}")
+  end
+
+
 end
