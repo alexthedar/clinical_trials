a0170a4e1c21394ecb39620f0f011023e475d176
Alex Caste <alex.castaneda@gmail.com>
2015-05-21 22:07:44 -0700

    all_vacation_days returns array of all days a specialist is currently unavailable

--- lib/vacation.rb
+++ lib/vacation.rb
@@ -14,17 +14,18 @@ class Vacation < ActiveRecord::Base
     vacation_dates
   end
 
-define_singleton_method(:all_vacation_days) do |id|
-  all_dates = []
-  @specialist = Specialist.find(id)
-  @specialist_vacations = @specialist.vacations
-   @specialist_vacations.each do |single_vacation|
-    dates = single_vacation.unavailable_dates
-     dates.each do |single_date|
-      all_dates.push(single_date)
+  define_singleton_method(:all_vacation_days) do |id|
+    all_dates = []
+    @specialist = Specialist.find(id)
+    @specialist_vacations = @specialist.vacations
+     @specialist_vacations.each do |single_vacation|
+      dates = single_vacation.unavailable_dates
+       dates.each do |single_date|
+        all_dates.push(single_date)
+      end
     end
+  all_dates
   end
-all_dates
-end
+
 
 end
